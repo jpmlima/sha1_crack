a5c6909f71a9d9d398f1fb655293a1c02a389893
jpmlima <joaolima@ua.pt>
2020-06-16 10:00:51 +0100

    Update sha1.py

--- sha1.py
+++ sha1.py
@@ -10,7 +10,7 @@ ch_symbol1 = '!@#$%^&*()_+-=,.'
 ch_symbol2 = """`~[]\{}|;':"/<>?"""
 
 # hex_dig = raw_input('SHA1: ')				
-hex_dig = '54AE9735798588D2B67EB97DD90C542E485EAA59'							
+hex_dig = '####'							
 								
 generator = wordlist.Generator(ch_alpha_l + ch_alpha_u + ch_numbers + ch_symbol1)
 for each in generator.generate(1, 20):	
